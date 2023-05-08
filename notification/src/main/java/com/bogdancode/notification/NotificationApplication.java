ba6234af275c4f4211a9a15be1aaf2e4762f88f7
bogdanMierloiu <bogdy.musiclover@gmail.com>
2023-05-08 07:04:37 +0300

    kafka impl

---
+++ notification/src/main/java/com/bogdancode/notification/NotificationApplication.java
@@ -0,0 +1,24 @@
+package com.bogdancode.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        // send out an email notification
+        log.info("Received notification for Order - {}", orderPlacedEvent.getOrderNumber());
+    }
+
+}
+
+
